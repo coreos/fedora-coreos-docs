3ea1149ad3433ffb8e28645fb8f7d8b9e1adead1
Hristo Marinov <hricky@mail.bg>
2025-02-07 14:33:14 +0200

    update-versions.py: Auto update latest Butane spec
    - Add an attribute for the latest version of the Ignition specs
    - Automatically update the butane and ignition spec attributes by
      synchronizing them with Butane specs docs

--- ci/update-versions.py
+++ ci/update-versions.py
@@ -5,6 +5,7 @@ import os
 import requests
 import sys
 import yaml
+import re
 
 GITHUB_RELEASES = {
     'butane-version': 'coreos/butane',
@@ -12,6 +13,11 @@ GITHUB_RELEASES = {
 }
 FCOS_STREAMS = {
     'stable-version': 'stable',
+}
+
+STABLE_SPECS = {
+    'butane-latest-stable-spec': 'coreos/butane',
+    'ignition-latest-stable-spec': 'coreos/ignition',
 }
 
 basedir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
@@ -39,8 +45,22 @@ with open(os.path.join(basedir, 'antora.yml'), 'r+') as fh:
         # artifact type, but the website doesn't do that either
         attrs[attr] = resp.json()['architectures']['x86_64']['artifacts']['metal']['release']
 
+    for attr, repo in STABLE_SPECS.items():
+        headers = {'Authorization': f'Bearer {github_token}'} if github_token else {}
+        resp = requests.get(
+            f'https://raw.githubusercontent.com/{repo}/refs/heads/main/docs/specs.md',
+            headers=headers
+        )
+        resp.raise_for_status()
+        if attr == 'butane-latest-stable-spec':
+            line_match = re.search(r'\s*-.+\(config-fcos.+\)', resp.text, re.MULTILINE)
+            attrs[attr] = re.search(r'(\d\.){2}\d', line_match[0])[0]
+        elif attr == 'ignition-latest-stable-spec':
+            line_match = re.search(r'\s*-.+\(configuration.+\)', resp.text, re.MULTILINE)
+            attrs[attr] = re.search(r'(\d\.){2}\d', line_match[0])[0]
+
     if attrs != orig_attrs:
         fh.seek(0)
         fh.truncate()
-        fh.write("# Automatically modified by update-versions.py; comments will not be preserved\n\n")
+        fh.write('# Automatically modified by update-versions.py; comments will not be preserved\n\n')
         yaml.safe_dump(config, fh, sort_keys=False)
