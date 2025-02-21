a6ef887862eb3e3b586316ea53223c174977d58e
Hristo Marinov <hricky@mail.bg>
2025-02-21 13:32:10 +0200

    Auto update latest Butane and Ignition specs
    Add an attribute for the latest version of the Ignition specs.
    
    Automatically update the latest Butane and Ignition specs
    attributes by synchronizing them with the corresponding docs.

--- ci/update-versions.py
+++ ci/update-versions.py
@@ -4,16 +4,20 @@
 import os
 import requests
 import sys
-import base64
 import yaml
+import base64
 
 GITHUB_RELEASES = {
     'butane-version': 'coreos/butane',
-    'butane-latest-stable-spec': 'coreos/butane',
     'ignition-version': 'coreos/ignition',
 }
 FCOS_STREAMS = {
     'stable-version': 'stable',
+}
+
+STABLE_SPECS = {
+    'butane-latest-stable-spec': 'coreos/butane',
+    'ignition-latest-stable-spec': 'coreos/ignition',
 }
 
 basedir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
@@ -26,21 +30,13 @@ with open(os.path.join(basedir, 'antora.yml'), 'r+') as fh:
 
     for attr, repo in GITHUB_RELEASES.items():
         headers = {'Authorization': f'Bearer {github_token}'} if github_token else {}
-        if attr == 'butane-version' or attr == 'ignition-version':
-            resp = requests.get(
-                f'https://api.github.com/repos/{repo}/releases/latest',
-                headers=headers
-            )
-            resp.raise_for_status()
-            tag = resp.json()['tag_name']
-            attrs[attr] = tag.lstrip('v')
-        elif attr == 'butane-latest-stable-spec':
-            resp = requests.get(
-                f'https://api.github.com/repos/{repo}/contents/docs/specs.md',
-                headers=headers
-            )
-            resp.raise_for_status()
-            attrs[attr] = ''.join(str(base64.b64decode(resp.json()['content'])).split('- ', 3)[2:3])[2:-26]
+        resp = requests.get(
+            f'https://api.github.com/repos/{repo}/releases/latest',
+            headers=headers
+        )
+        resp.raise_for_status()
+        tag = resp.json()['tag_name']
+        attrs[attr] = tag.lstrip('v')
 
     for attr, stream in FCOS_STREAMS.items():
         resp = requests.get(f'https://builds.coreos.fedoraproject.org/streams/{stream}.json')
@@ -48,6 +44,18 @@ with open(os.path.join(basedir, 'antora.yml'), 'r+') as fh:
         # to be rigorous, we should have a separate attribute for each
         # artifact type, but the website doesn't do that either
         attrs[attr] = resp.json()['architectures']['x86_64']['artifacts']['metal']['release']
+
+    for attr, repo in STABLE_SPECS.items():
+        headers = {'Authorization': f'Bearer {github_token}'} if github_token else {}
+        resp = requests.get(
+            f'https://api.github.com/repos/{repo}/contents/docs/specs.md',
+            headers=headers
+        )
+        resp.raise_for_status()
+        if attr == 'butane-latest-stable-spec':
+            attrs[attr] = ''.join(str(base64.b64decode(resp.json()['content'])).split('- ', 3)[2:3])[2:-26]
+        elif attr == 'ignition-latest-stable-spec':
+            attrs[attr] = ''.join(str(base64.b64decode(resp.json()['content'])).split('- ', 3)[1:2])[2:-26]
 
     if attrs != orig_attrs:
         fh.seek(0)
